5fe3d788443fefe3921ed76709d35d5df4eac2de
n2x4 <n2x4@users.noreply.github.com>
2026-03-05 15:48:16 +0000

    Fix Meshtastic map coordinates to Northeast Ohio (Cleveland)

--- src/pages/meshtastic.js
+++ src/pages/meshtastic.js
@@ -61,7 +61,7 @@ export default function Home() {
           <section id="map-section" className="map-section">
           <div className="container">
             <h2><a href="https://map.neome.sh" target="_blank" rel="noopener noreferrer">Submitted Node Map</a></h2>
-            <iframe id="meshcore-map-iframe" src="https://meshtastic.liamcottle.net/?lat=42.379345257263964&lng=288.52706909179693&zoom=10" title="description_of_embedded_content" width="100%" height="600"></iframe>
+            <iframe id="meshcore-map-iframe" src="https://meshtastic.liamcottle.net/?lat=41.4993&lng=278.3056&zoom=9" title="description_of_embedded_content" width="100%" height="600"></iframe>
           </div>
         </section>
 
