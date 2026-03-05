78b812f3a1a800b083795704c1719c9159d0ecc8
n2x4 <n2x4@users.noreply.github.com>
2026-03-05 16:04:53 +0000

    Switch to custom domain neome.sh (url + baseUrl)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,10 +28,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://n2x4.github.io',
+  url: 'https://neome.sh',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/neomesh/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
